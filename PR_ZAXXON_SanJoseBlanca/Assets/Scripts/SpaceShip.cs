77b6bf3221873fd718ae45e7d0471cc57d1ee1b6
BlancaSJG <77485293+BlancaSJG@users.noreply.github.com>
2021-10-01 19:58:09 +0200

    Rotación nave
    He usado un quaternion para hacerla rotar con un poco de ángulo

--- PR_ZAXXON_SanJoseBlanca/Assets/Scripts/SpaceShip.cs
+++ PR_ZAXXON_SanJoseBlanca/Assets/Scripts/SpaceShip.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class SpaceShip : MonoBehaviour
 {
+
+
+    //Variables para desplazamiento
     [SerializeField] float desplSpeed;
 
     float limiteR = 14;
     float limiteL = -14;
     float limiteU = 11;
     float limiteD = -1;
+   
+
+    //Variables para rotacion
+    float rotAng = -6f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         desplSpeed = 4f;
+        
     }
 
     // Update is called once per frame
@@ -23,24 +31,7 @@ public class SpaceShip : MonoBehaviour
     {
 
 
-        // Control de movimiento con teclas
-
-        /*if (Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.UpArrow)) // Frente
-        {
-            this.transform.Translate(Vector3.up * desplSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.DownArrow)) // Después
-        {
-            this.transform.Translate(Vector3.up * -desplSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow)) // Izquierda
-        {
-            this.transform.Translate(Vector3.right * -desplSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow)) // Derecha
-        {
-            this.transform.Translate(Vector3.right * desplSpeed * Time.deltaTime);
-        }*/
+        // Control de movimiento 
 
         float desplH = Input.GetAxis("Horizontal");
         float desplV = Input.GetAxis("Vertical");
@@ -49,12 +40,10 @@ public class SpaceShip : MonoBehaviour
         float posX = transform.position.x;
         float posY = transform.position.y;
 
-
         transform.Translate(Vector3.right * desplSpeed * desplH * Time.deltaTime, Space.World);
         transform.Translate(Vector3.up * desplSpeed * desplV * Time.deltaTime, Space.World);
 
         //Restriccion de movimiento 
-
         if(posX >= limiteR && desplH > 0 || posX <= limiteL && desplH < 0 || posY >= limiteU && desplV > 0 || posY <= limiteD && desplV < 0)
         {
             desplSpeed = 0f;
@@ -64,8 +53,17 @@ public class SpaceShip : MonoBehaviour
             desplSpeed = 4f;
         }
 
-        
 
+
+        // Rotacion con el movimiento
+
+        float rotZ = Input.GetAxis("Horizontal") * rotAng;
+        float rotX = Input.GetAxis("Vertical") * rotAng;
+
+        // calculo de rotacion mediante quaternion
+        Quaternion target = Quaternion.Euler(rotX, 0, rotZ);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
 
 
 
